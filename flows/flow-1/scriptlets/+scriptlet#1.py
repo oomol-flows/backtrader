e7fb1b06ce685ada34f8fc86e28baab59035dd20
shaun <beingswu@gmail.com>
2025-06-24 07:06:41 +0000

    init back-trader

---
+++ flows/flow-1/scriptlets/+scriptlet#1.py
@@ -0,0 +1,15 @@
+from oocana import Context
+
+#region generated meta
+import typing
+class Inputs(typing.TypedDict):
+    input: str
+class Outputs(typing.TypedDict):
+    output: typing.Any
+#endregion
+
+def main(params: Inputs, context: Context) -> Outputs:
+
+    # your code
+
+    return { "output": "output_value" }
